181e25ab02f6b25a71d0f64b650b3b26a5ca090e
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2023-02-07 21:48:47 +0530

    Add files via upload

---
+++ Debug-Explain/Week-1/ques2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int  main() { // we can use void as return type but standard c++ complier wont understand it
+     short i=2300, j=4322;
+     cout<<"i+j="<<(i+j); // cout must have << operator and there been asked to 
+                          // find the sum of i and j no use of - sign
+     return 0;
+}
