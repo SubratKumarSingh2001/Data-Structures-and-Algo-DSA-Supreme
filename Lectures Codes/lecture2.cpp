65bdc732673aeea84d0a80f5828e04f969427055
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2023-02-11 21:47:51 +0530

    Add files via upload

---
+++ Lectures Codes/lecture2.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int c= 5;
+    cout<<(++c)*(++c);
+
+    //int a = 5;
+    //int b = 3;
+
+    // cout << (a<=b) << endl;
+
+    //cout << !(a==5);
+    
+
+    
+    // cout << (5)/((double)3);
+    
+    // int a = 5;
+    // int b = 3;
+
+    // cout << a+b << endl;
+    // cout << a-b << endl;
+    // cout << a*b << endl;
+    // cout << a%b << endl;
+    // cout << a/b << endl;
+    
+    // double d = 5.7;
+
+    // int x = (int)d ;
+    // cout << "value of x is " << x << endl;
+    
+    // char ch = 97;
+    // cout << ch << endl;
+
+    // int num = 'b';
+    // cout << num << endl;
+    
+    // long long a = 5;
+    // cout << sizeof(a) << endl;
+    
+    // int a = 5;
+    // cout <<" value of a is : " << a << endl;
+
+    // char ch = 256;
+    // cout <<" value of ch is : " << ch << endl;
+
+    // bool flag = 1;
+    // cout << "value of flag is " << flag << endl;
+    
+    // cout <<  "Namaste Bharat\n" << endl ;
+    // cout << '\n';
+
+    // cout << "Enter a number: " << endl;
+
+    // int a;
+    // //taking input
+    // cin >> a;
+
+    // cout << "You entered " << a << endl;
+    
+}
