da73f36a827a15b2c8f2105cdcc0b2905c75f6c5
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-01-23 19:26:34 +0530

    Update majorityelement.cpp

--- Homework+Classwork/Vectors-11-02-23/majorityelement.cpp
+++ Homework+Classwork/Vectors-11-02-23/majorityelement.cpp
@@ -2,21 +2,49 @@
 #include <vector>
 using namespace std;
 
-int findmajorityele(vector<int>arr) {
-    for(int i=0; i<arr.size(); i++) {
-        int count =1;
-        for(int j=i+1; j<arr.size(); j++) {
-            if(arr[i]==arr[j]) {
+int findMajorityEle(vector<int> &arr) {
+    // Method:1 Brute Force Approach TC:O(n^2) and SC:O(1);
+    for (int i = 0; i < arr.size(); i++) {
+        int count = 1;
+        for (int j = i + 1; j < arr.size(); j++) {
+            if (arr[i] == arr[j]) {
                 count++;
             }
         }
-        if(count>arr.size()/2) {
-            cout<<"The majority occurance of an element is : "<<arr[i];
-            break;
+        if (count > arr.size() / 2) {
+            return arr[i];
         }
-        else{
-            cout<<"There is no majority element";
+    }
+
+    // Method:2 Optimal Approach (Boyer-More's Voting Algorithm) TC:O(n) and SC:O(1);
+    int ele = 0, count = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        if (count == 0) {
+            ele = arr[i];
+            count++;
         }
+        else if (arr[i] == ele) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+    /*
+    till of now we have got our potential majority element to check whether it is majority element we will find the no.of occurrence
+    of ele in array and if it is >size/2 then that potential element is our resultant majority ele
+    */
+    int cnt = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == ele) {
+            cnt++;
+        }
+    }
+    if (cnt > arr.size() / 2) {
+        return ele;
+    }
+    else {
+        return -1;
     }
 }
 
